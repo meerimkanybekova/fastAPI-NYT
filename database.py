7dfd0ea384b1111951b15d8b1b69cdb0da2fd0d1
Meerim Kanybekova <kanybekova_m@auca.kg>
2025-06-16 19:44:10 +0600

    add postgres db

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
+import os
 
-engine = create_engine("sqlite:///./chat.db", connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def init_db():
